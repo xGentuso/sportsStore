8970e87f80edd61063bb609a66e3ad5c276dc311
xGentuso <ryan.mota@triosstudent.com>
2024-10-28 09:20:54 -0400

    added pagination

--- SportsStore/src/app/store/store.component.ts
+++ SportsStore/src/app/store/store.component.ts
@@ -7,14 +7,38 @@ import { ProductRepository } from "../model/product.repository";
     templateUrl: "store.component.html"
 })
 export class StoreComponent {
- 
+    selectedCategory: string | undefined;
+    productsPerPage = 4;
+    selectedPage = 1;
+
     constructor(private repository: ProductRepository) { }
- 
+
     get products(): Product[] {
-        return this.repository.getProducts();
+        let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
+        return this.repository.getProducts(this.selectedCategory)
+            .slice(pageIndex, pageIndex + this.productsPerPage);
     }
- 
+
     get categories(): string[] {
         return this.repository.getCategories();
+    }
+
+    changeCategory(newCategory?: string) {
+        this.selectedCategory = newCategory;
+    }
+
+    changePage(newPage: number) {
+        this.selectedPage = newPage;
+    }
+
+    changePageSize(newSize: number) {
+        this.productsPerPage = Number(newSize);
+        this.changePage(1);
+    }
+
+    get pageNumbers(): number[] {
+        return Array(Math.ceil(this.repository
+            .getProducts(this.selectedCategory).length / this.productsPerPage))
+                .fill(0).map((x, i) => i + 1);
     }
 }
